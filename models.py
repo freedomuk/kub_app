f669ae2e7fd8f1f8bd99fffc2aba56007af92e15
Kasyan Roman <kasanr223@gmail.com>
2026-06-19 10:08:35 +0300

    ddd

--- models.py
+++ models.py
@@ -10,6 +10,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
+
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer,primary_key=True)
